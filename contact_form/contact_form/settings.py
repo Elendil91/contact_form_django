3b177ca9e3da0a5fe2167a9c6ee6d8c397b267ef
Simbelmyne <simbelmyne.evermind.131717@gmail.com>
2022-07-09 19:27:37 -0400

    fixing secret key

--- contact_form/contact_form/settings.py
+++ contact_form/contact_form/settings.py
@@ -1,3 +1,4 @@
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c@x9*#k+rw53xv63ujlf7_^_wd!86@r6(=k2^pu$wn*t#^im4*'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
